bb046efae5b8d27d4e3f4faf46c0cf9b45632a8f
Baraa <178164194+AlbaraaAloush@users.noreply.github.com>
2026-05-13 13:12:26 +0300

    feat: implement playGame method

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+function createPlayer(name, mark) {
+    let score = 0;
+    const getScore = () => score;
+    const incrementScore = () => score++;
+    const resetScore = () => {score = 0};
+
+    return {name, mark, getScore, incrementScore, resetScore};
+}
+
+// responsible for handling board moves only
+
 const GameBoard = (() =>{
     let gameBoard = ['', '', '', '', '', '', '', '', ''];
 
     const winPatterns = [
-    [0,1,2], [3,4,5], [6,7,8], // rows
-    [0,3,6], [1,4,7], [2,5,8], // columns
-    [0,4,8], [2,4,6]           // diagonals
+    [0,1,2], [3,4,5], [6,7,8], 
+    [0,3,6], [1,4,7], [2,5,8], 
+    [0,4,8], [2,4,6]           
     ];
 
     const getGameBoard = () => gameBoard;
     
-    const playMove = (symbol, index) =>{
-        if(index >= 0 && index <= 8 && gameBoard[index] === '' && gameBoard.length <= 9){
-            gameBoard[index] = symbol;
-            return true;
+    const playMove = (mark, index) =>{
+        if(index >= 0 && index <= 8 && gameBoard[index] === ''){
+            gameBoard[index] = mark;
+            return checkWinner();
+        } else{
+            throw new Error(`${index+1} position is either preoccupied or out of range`);
         }
-        return false;
     };
 
     const checkWinner = () =>{
@@ -25,43 +37,45 @@ const GameBoard = (() =>{
                 return "o";
             } else if(!gameBoard.includes('')){
                 return "draw";
-            }
+            } 
         }
+        return "ongoing";
     };
 
-    const endOfRound = () => {
-        return !gameBoard.includes('');
-    }
+    const reset = () => {gameBoard = ['', '', '', '', '', '', '', '', '']};
 
-    const playRound = () => {
-        do{
-            playMove()
-
-
-        } while(!endOfRound);
-    }
-
-
-    return {getGameBoard, playMove, checkWinner};
+    return {getGameBoard, playMove, reset};
 })();
 
-// function createPlayer(mark) {
-//     let score = 0;
-//     const getMark = () => mark; 
-//     const getScore = () => score;
-//     const incrementScore = () => score++;
-//     const resetScore = () => {score = 0};
+const p1 = createPlayer("Salem" ,"x");
+const p2 = createPlayer ("Emad", "o");
 
-//     return {getMark, getScore, incrementScore, resetScore};
-// }
-
-// const GameController = ((firstPlayer, secondPlayer, numberOfRounds) => {
-//     const playGame = (numberOfRounds) =>{
-//         for(let i = 0; i <= numberOfRounds; i++){
-//             gameBoard.playRound(firstPlayer, secondPlayer);
-//         }
-//     }
-
-
-
-// })();
+const GameController = ((firstPlayer, secondPlayer) => {
+    let currentPlayer = firstPlayer;
+    let nextPlayer = secondPlayer;
+    let currentRound = 0;
+    let result;
+    const playGame = () => {
+        GameBoard.reset();
+        do {
+            alert(`${currentPlayer.name} ${currentPlayer.mark} turn!`);
+            result = GameBoard.playMove(currentPlayer.mark, prompt(`Enter the position for ${currentPlayer.name}: `));
+            if(result === firstPlayer.mark){
+                firstPlayer.incrementScore();
+                console.log(`result ${firstPlayer.name} wins, first Player: ${firstPlayer.name} ${firstPlayer.getScore()}, Second Player: ${firstPlayer.mark} ${secondPlayer.getScore()} Board: ${GameBoard.getGameBoard()}`);
+                return true;
+            } else if(result === secondPlayer.mark){
+                secondPlayer.incrementScore();
+                console.log(`result ${secondPlayer.name} wins, first Player: ${firstPlayer.mark} ${firstPlayer.getScore()}, Second Player: ${firstPlayer.mark} ${secondPlayer.getScore()} Board: ${GameBoard.getGameBoard()}`);
+                return true;
+            } else if (result === "draw"){
+                console.log(`result draw, first Player: ${firstPlayer.mark} ${firstPlayer.getScore()}, Second Player: ${firstPlayer.mark} ${secondPlayer.getScore()} Board: ${GameBoard.getGameBoard()}`);
+                return true;
+            } 
+            let temp = currentPlayer;
+            currentPlayer = nextPlayer;
+            nextPlayer = temp;
+        } while(result === "ongoing");
+    };
+    return {playGame};
+})(p1, p2);
